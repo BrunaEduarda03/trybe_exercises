694ecbab848a2cd76f59c0b85bf744cd431152e6
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2022-02-04 19:16:10 -0300

    atualizado ex_08 e finalizado ex_09

---
+++ fundamentos/dia-1-javascript-primeiros-passos/exercicio_08.js
@@ -0,0 +1,5 @@
+const num1 = 1, num2 = 3, num3 = 9;
+
+if(num1 % 2 === 0 ||num2 % 2 === 0|| num3 % 2 === 0)
+    console.log('true');
+console.log('false');
---
+++ fundamentos/dia-1-javascript-primeiros-passos/exercicio_09.js
@@ -0,0 +1,6 @@
+const num1 = 2, num2 = 4, num3 = 9;
+let retorno = false;
+if((num1 % 2 !== 0 ||num2 % 2 !== 0|| num3 % 2 !== 0))
+    retorno = true;
+
+console.log(retorno);
