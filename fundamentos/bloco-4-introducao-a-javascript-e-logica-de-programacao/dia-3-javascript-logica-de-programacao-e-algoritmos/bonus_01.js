3f650fabfab0e99c4cfe841244aa8415284aef36
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2022-02-14 11:28:31 -0300

    adicionar bonus 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus_01.js
@@ -0,0 +1,12 @@
+let n =5;
+
+for(let i=0;i<n;i++){
+    for(let j=0;j<i;j++){
+        console.log('*');
+        if(n === i){
+            console.log('\n');
+        }
+            
+    }
+    
+}
