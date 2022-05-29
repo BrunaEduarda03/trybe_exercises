a3c84ae235637d6ebd898b69b6f50ef2aa69abd1
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2022-05-28 22:31:37 -0300

    continuação do exercicio de fixação feito no momento assincrono

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-fixacao/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-fixacao/src/App.js
@@ -1,12 +1,11 @@
 import DadJoke from './DadJoke';
-import Counter from './Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <DadJoke />
-      <Counter />
+   
     </div>
   );
 }
--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-fixacao/src/DadJoke.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicio-fixacao/src/DadJoke.js
@@ -3,7 +3,7 @@ import React from 'react';
 class DadJoke extends React.Component {
   constructor() {
     super();
-
+    this.fetchJoke = this.fetchJoke.bind(this);
     this.saveJoke = this.saveJoke.bind(this);
 
     this.state = {
@@ -14,39 +14,61 @@ class DadJoke extends React.Component {
   }
 
   async fetchJoke() {
-    const requestHeaders = { headers: { Accept: 'application/json' } }
-    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-    const requestObject = await requestReturn.json();
-    console.log(requestObject);
+    this.setState(
+      {loading:true}, // Primeiro parâmetro da setState()!
+      async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } }
+        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+        const requestObject = await requestReturn.json();
+        // console.log(requestObject);
     this.setState({
+      loading:false,
       jokeObj: requestObject,
-    })
-  }
+    });
+  });
+}
 
-  componentDidMount() {
+componentDidMount() {
     this.fetchJoke();
   }
 
   saveJoke() {
     //Esse método será responsável por salvar a piada no array de piadas storedJokes!!
+    //this.setState(({ meuArrayNoEstado }) => ({meuArrayNoEstado: [...meuArrayNoEstado, novoElemento] }))
+    this.setState(({storedJokes,jokeObj}) => ({
+      storedJokes: [...storedJokes,jokeObj]
+    }));
+    this.fetchJoke();
   }
 
+  
+
+  renderJokeElement() {
+    return (
+      <div>
+        <p>{this.state.jokeObj.joke}</p>
+        <button 
+        type='button'
+        onClick={this.saveJoke}
+        >
+        Salvar a piada!
+        </button>
+      </div>
+      
+    )
+    
+    }
   render() {
-    const { storedJokes } = this.state;
-    // const loadingElement = <span>Loading...</span>;
+    const { storedJokes,jokeObj, loading } = this.state;
+    const loadingElement = <span>Loading...</span>;
     return (
       <div>
         <span>
           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
-         
-        </span>
-        
+        </span> 
+        <p>{ loading ? loadingElement : this.renderJokeElement() }</p>
       {
         /*
-        Aqui vamos construir nossa lógica com uma renderização condicional
-        do nosso componente Joke, a ideia é renderizar um loading enquanto
-        esperamos a nossa requisição de piadas finalizar.
-
         <p>RENDERIZAÇÃO CONDICIONAL</p>
         */
       }
